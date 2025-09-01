1cd8a1ff27dcab951b748d22b34930806418106c
Hammad Liaquat <154601362+hammadliaquat77@users.noreply.github.com>
2025-09-01 05:05:57 +0500

    updared

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,36 +1,12 @@
-// import { useEffect } from "react";
-// import { useLocation } from "react-router-dom";
+ import { useEffect } from "react";
+ import { useLocation } from "react-router-dom";
 
-// export default function ScrollToTop() {
-//   const { pathname } = useLocation();
-
-//   useEffect(() => {
-//     window.scrollTo(0, 0);
-//   }, [pathname]);
-
-//   return null;
-// }
-
-
-
-
-
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-
-function ScrollToHash() {
-  const { hash } = useLocation();
-
+ export default function ScrollToTop() {
+   const { pathname } = useLocation();
   useEffect(() => {
-    if (hash) {
-      const el = document.getElementById(hash.replace("#", ""));
-      if (el) {
-        el.scrollIntoView({ behavior: "smooth" });
-      }
-    }
-  }, [hash]);
+     window.scrollTo(0, 0);
+   }, [pathname]);
 
-  return null;
-}
+   return null;
+ }
 
-export default ScrollToHash;
